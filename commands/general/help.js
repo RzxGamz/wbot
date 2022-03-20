23520e9c66e8fc936ee9beda7e091f653715f56c
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-03-20 15:06:48 +0700

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -2,7 +2,7 @@ const djs = require('@discordjs/collection');
 const con = require('../../core/connect');
 const crypto = require('crypto')
 const ev = con.Whatsapp;
-const moment = require('moment-timezone');
+const moment = require('moment-timezone')
 const time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 const ucapan = "Selamat "+ moment(Date.now()).tz('Asia/Jakarta').locale('id').format('a')
 
@@ -88,8 +88,8 @@ module.exports = {
       //let loc = await ev.prepareMessage(msg.from, require('fs').readFileSync('././temp/img.png'), 'locationMessage', { thumbnail: require('fs').readFileSync('././temp/img.png') })
       let rMime = ["application/pdf","application/msword","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
       let randomMime = rMime[Math.floor(Math.random() * rMime.length)]
-      let doc = { "url": "https://mmg.whatsapp.net/d/f/Ano5cGYOFQnC51uJaqGBWiCrSJH1aDCi8-YPQMMb1N1y.enc", "mimetype": randomMime, "title": "@ShellsBot", "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=", "fileLength": 50000000000, "pageCount": 404, "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=", "fileName": "sʜᴇʟʟ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ", "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=", "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC", "mediaKeyTimestamp": "1634472176", "jpegThumbnail": require('fs').readFileSync('././temp/img.png') }
-      ev.sendMessage(msg.from, { documentMessage: doc, contentText: str, footerText: "Shell Bot", buttons: buttons, headerType: "DOCUMENT" }, "buttonsMessage", { quoted: msg, thumbnail: require('fs').readFileSync('././temp/img.png'), contextInfo: { externalAdReply: { title: "Shell Bot", body: "", mediaType: "2", thumbnail: require('fs').readFileSync('././temp/thb.png'), mediaUrl: `https://www.instagram.com` }, mentionedJid: [sender] }})
+      let doc = { "url": "https://mmg.whatsapp.net/d/f/Ano5cGYOFQnC51uJaqGBWiCrSJH1aDCi8-YPQMMb1N1y.enc", "mimetype": randomMime, "title": "@ShellsBot", "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=", "fileLength": 50000000000, "pageCount": 404, "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=", "fileName": "sʜᴇʟʟ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ", "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=", "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC", "mediaKeyTimestamp": "1634472176", "jpegThumbnail": require('fs').readFileSync('././temp/img.jpg') }
+      ev.sendMessage(msg.from, { documentMessage: doc, contentText: str, footerText: "Shell Bot", buttons: buttons, headerType: "DOCUMENT" }, "buttonsMessage", { quoted: msg, contextInfo: { externalAdReply: { title: "Shell Bot", body: "", mediaType: "2", thumbnail: require('fs').readFileSync('././temp/thb.png'), mediaUrl: `https://www.instagram.com` }, mentionedJid: [sender] }})
     }
   },
 };
